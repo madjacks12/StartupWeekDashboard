63794acde7f243f08ca2360ed89f7c7ba642c5de
MJ <mdjacksonvideo@gmail.com>
2018-01-12 10:44:40 -0800

    wrote new routes for getting and posting new Events

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
+import models.Event;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -13,8 +15,28 @@ public class App {
 
         get("/", (request, response) ->{
             Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "templates/index.hbs");
-        }, new HandlebarsTemplateEngine() );
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/event/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "event-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("event/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String description = request.queryParams("description");
+            int year = Integer.parseInt(request.queryParams("year"));
+            int day = Integer.parseInt(request.queryParams("day"));
+            String month = request.queryParams("month");
+            String attendee = request.queryParams("attendee");
+            Event newEvent = new Event(name, description, year, day, month, attendee);
+            model.put("newEvent", newEvent);
+            return new ModelAndView(model, "success.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
 
 
     }
